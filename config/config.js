0615aabc241619445cca935e2657bc25d4af8a3c
kelly <korevec@gmail.com>
2024-11-16 08:46:45 -0800

    init

---
+++ config/config.js
@@ -0,0 +1,12 @@
+const layout = [
+  [1,2]
+]
+const devices = [{
+  path: '/dev/tty.usb',
+  addresses: [1, 2],
+  baudRate: 57600
+}]
+const options = {
+  isMirrored: true
+}
+export { layout, devices, options }
